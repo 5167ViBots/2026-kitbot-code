b1503a5726b731d6c1c3d8ba42631b8c332331fe
Tommy <davidtklingerman@gmail.com>
2026-02-12 22:56:36 -0500

    Fixup missing Drive subsystem, Added Indexer subsystem, bound intake to X, and set the drive subsystem to default drive by controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,10 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
 import frc.robot.commands.*;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -23,6 +24,8 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+  private final IndexerSubsystem indexerSubsystem = new IndexerSubsystem();
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
 
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -52,8 +55,10 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    m_driverController.a().whileTrue(new ShooterCommand(shooterSubsystem));
-    
+    m_driverController.a().whileTrue(new ShooterCommand(shooterSubsystem, indexerSubsystem));
+    m_driverController.x().whileTrue(new IntakeCommand(shooterSubsystem, indexerSubsystem));
+
+    driveSubsystem.setDefaultCommand(new DriveCommand(driveSubsystem,m_driverController));
 
   }
 
